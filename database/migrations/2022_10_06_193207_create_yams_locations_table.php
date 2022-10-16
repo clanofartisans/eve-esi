a0d2f65467bbfbc2bfcc061e885c798793c4bedf
Brad Turner <brad@clanofartisans.com>
2022-10-16 14:56:25 -0500

    Renamed the locations table to yams_table.

---
+++ database/migrations/2022_10_06_193207_create_yams_locations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('yams_locations', function (Blueprint $table) {
+            $table->unsignedBigInteger('region_id');
+            $table->string('region_name');
+            $table->unsignedBigInteger('system_id');
+            $table->string('system_name');
+            $table->enum('location_type', ['station', 'structure']);
+            $table->unsignedBigInteger('location_id')->primary();
+            $table->string('name');
+            $table->decimal('security_status', 2, 1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('yams_locations');
+    }
+};
